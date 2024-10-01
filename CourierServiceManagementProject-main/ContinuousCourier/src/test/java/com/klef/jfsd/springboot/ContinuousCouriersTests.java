9dfb7412ae278993f3544b516544d4465193a509
Sasupalli Rohith <96166239+sasupallirohith@users.noreply.github.com>
2024-10-01 17:57:59 +0530

    Update ContinuousCouriersTests.java

--- CourierServiceManagementProject-main/ContinuousCourier/src/test/java/com/klef/jfsd/springboot/ContinuousCouriersTests.java
+++ CourierServiceManagementProject-main/ContinuousCourier/src/test/java/com/klef/jfsd/springboot/ContinuousCouriersTests.java
@@ -1,13 +1,11 @@
 package com.klef.jfsd.springboot;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
-class ContinuousCouriersTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+@SpringBootApplication
+public class ContinuousCouriers {
+    public static void main(String[] args) {
+        SpringApplication.run(ContinuousCouriers.class, args);
+    }
 }
